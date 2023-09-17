0ff8b3f94b6a7cb7c8ce6d0c9c8cb6932bff19a2
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-09-17 17:19:14 +0300

    Pagination example.

---
+++ 05 Pagination/main.js
@@ -0,0 +1,43 @@
+// ---------- get data from the server
+const api = 'https://dummyjson.com/products';
+
+const pagination = {
+    limit: 3,
+    skip: 0,
+    total: null,
+
+    next() {
+        this.skip += this.limit;
+    }
+};
+
+async function getDataFromServer(url) {
+
+    const urlWithParams = url += `?limit=${pagination.limit}&skip=${pagination.skip}`;
+
+    // ----- send request to the server
+    // HTTP GET: api
+    const response = await fetch(urlWithParams);
+
+    console.log("Status:", response.status); // 200: OK
+
+    const data = await response.json();
+    console.log(data);
+    
+    pagination.total = data.total;
+    pagination.next();
+
+    for (const i of data.products) {
+        
+        userList.innerHTML += ` <li class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+                                    ${i.title}
+                                    <span class="badge bg-primary rounded-pill">${i.price}$</span>
+                                </li>`;
+    }
+}
+
+getDataFromServer(api);
+
+moreBtn.onclick = () => {
+    getDataFromServer(api);
+};
